8df223c142790d30c1cf054b87e63bd3f57104bb
MOHIT . <mohit.24scse1420017@galgotiasuniversity.ac.in>
2026-09-15 20:30:57 +0530

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int[] leftmax = new int[n];
+
+        leftmax[0] = height[0];
+        for(int  i = 1;i<height.length;i++){
+           leftmax[i] = Math.max(leftmax[i-1],height[i]);
+        }
+        int[] rightmax = new int[n];
+        rightmax[n-1] = height[n-1];
+        for(int i = n-2;i>=0;i--){
+            rightmax[i] = Math.max(rightmax[i+1],height[i]);
+        }
+
+       int totalwater = 0;
+
+       for(int i = 0 ; i< height.length;i++){
+            int waterLevel = Math.min(leftmax[i],rightmax[i]);
+              int water = waterLevel - height[i];
+            
+            if(water > 0){
+                totalwater = totalwater + water;
+            }
+       } 
+       return totalwater;
+    }
+}
