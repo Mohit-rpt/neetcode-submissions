efed92bc6349166cac44b7343fe16c4163214e4d
MOHIT . <mohit.24scse1420017@galgotiasuniversity.ac.in>
2026-09-18 21:04:44 +0530

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int left = 0;
+        int right = 0;
+        int windowSize = 0;
+        int s1Len = s1.length();
+
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+
+        for(int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i) - 'a']++;
+        }
+
+        while(right < s2.length()) {
+            freq2[s2.charAt(right) - 'a']++;
+
+            windowSize = right - left + 1;
+
+            if(windowSize > s1Len) {
+                freq2[s2.charAt(left) - 'a']--;
+                left++;
+            }
+
+            if(Arrays.equals(freq1, freq2)) {
+                return true;
+            }
+
+            right++;
+        }
+
+        return false;
+    }
+}
